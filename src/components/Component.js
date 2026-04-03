b9ecd6ac089bbab3b8e7ea9ac7fe5cfdbab1f26d
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-04-03 13:17:27 -0300

    Ajustando o react para aparecer o componente

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,9 +1,11 @@
-function Component() {  
-    <div>
-      <h1>Componente</h1>
-      <p>Estudar semrpe é fundamental para aprender React e desenvolver habilidades em programação.</p>
-      <p>Crescer também é aprender novas habilidades e conehcer o novo</p>
-    </div>
+ function Component() {  
+    return(
+     <div>
+        <h1>Componente</h1>
+        <p>Estudar semrpe é fundamental para aprender React e desenvolver habilidades em programação.</p>
+        <p>Crescer também é aprender novas habilidades e conehcer o novo</p>
+      </div>
+    )
 }
 
 export default Component;
