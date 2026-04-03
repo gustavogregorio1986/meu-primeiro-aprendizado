0743d3d1d7aee39dadd3f34c18730f67135284fc
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-04-03 13:19:37 -0300

    Desenvolver o formulario no react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Component from './components/Component';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <h2>Bem vindo ao React</h2> 
       <Component /> 
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+function Form(){
+    return(
+       <>
+        <h2>Formulário</h2>
+        <form>
+            <input type="text" placeholder="Digite seu nome"/>
+            <input type="email" placeholder="Digite seu email"/>
+            <button type="submit">Enviar</button>
+        </form>
+       </>
+    )
+}
+
+export default Form;
