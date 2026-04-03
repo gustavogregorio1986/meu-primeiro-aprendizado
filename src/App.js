55631b442a8a382712a8fcb6bcf5356f317a0259
Miguel Marquiori <miguelmarkiori@outlook.com>
2026-04-03 13:07:47 -0300

    Desenvolver componentes para aprendizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Component from './components/Component';
 
 function App() {
   return (
     <div className="App">
-      <h2>Bem vindo ao React</h2>  
+      <h2>Bem vindo ao React</h2> 
+      <Component /> 
     </div>
   );
 }
---
+++ src/components/Component.js
@@ -0,0 +1,9 @@
+function Component() {  
+    <div>
+      <h1>Componente</h1>
+      <p>Estudar semrpe é fundamental para aprender React e desenvolver habilidades em programação.</p>
+      <p>Crescer também é aprender novas habilidades e conehcer o novo</p>
+    </div>
+}
+
+export default Component;
